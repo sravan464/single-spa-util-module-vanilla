666fc6758e4c7a09d295a57c41fbf1e1faea3db0
sravan kumar ganji <sravankumarganji@sravans-MacBook-Pro.local>
2021-09-21 08:33:31 -0400

    console-log to test

--- src/sravan-org-single-spa-util-module-vanilla.js
+++ src/sravan-org-single-spa-util-module-vanilla.js
@@ -1,2 +1,4 @@
 // Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
+export function publicApiFunction() {
+    console.log("hello from module")
+}
